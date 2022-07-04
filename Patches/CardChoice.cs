5dc0cc67f11fe54238d5bd7ffcaadd3b982eb3fb
Tess <tess@tessyac.com>
2022-07-03 21:04:46 -0400

    I did a dumb

--- Patches/CardChoice.cs
+++ Patches/CardChoice.cs
@@ -12,21 +12,21 @@ namespace RarityLib.Patches
     internal class CardChoicePatchGetRanomCard
     {
         [HarmonyPriority(Priority.First)]
-        private static bool Prefix(CardChoice __instance, ref GameObject __result)
+        private static bool Prefix(CardInfo[] cards, ref GameObject __result)
         {
             float num = 0f;
-            for (int i = 0; i < __instance.cards.Length; i++)
+            for (int i = 0; i < cards.Length; i++)
             {
-                num += RarityUtils.rarities[(int)__instance.cards[i].rarity].calculatedRarity * RarityUtils.GetCardRarityModifier(__instance.cards[i]);
+                num += RarityUtils.rarities[(int)cards[i].rarity].calculatedRarity * RarityUtils.GetCardRarityModifier(cards[i]);
             }
             float num2 = UnityEngine.Random.Range(0f, num);
 
-			for (int j = 0; j < __instance.cards.Length; j++)
+			for (int j = 0; j < cards.Length; j++)
             {
-                num2 -= RarityUtils.rarities[(int)__instance.cards[j].rarity].calculatedRarity * RarityUtils.GetCardRarityModifier(__instance.cards[j]);
+                num2 -= RarityUtils.rarities[(int)cards[j].rarity].calculatedRarity * RarityUtils.GetCardRarityModifier(cards[j]);
                 if (num2 <= 0f)
 				{
-                    __result = __instance.cards[j].gameObject;
+                    __result = cards[j].gameObject;
 					break;
 				}
 			}
